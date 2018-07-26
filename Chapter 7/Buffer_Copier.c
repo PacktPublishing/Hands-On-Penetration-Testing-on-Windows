7a1321786207b70d97ba55059740131835e66c5f
bramwellp <38234817+bramwellp@users.noreply.github.com>
2018-07-26 02:14:33 -0400

    Create Buffer_Copier.c

---
+++ Chapter 7/Buffer_Copier.c
@@ -0,0 +1,7 @@
+#include <string.h>
+#include <stdio.h>
+void main(int argc, char *argv[]) {
+    char buffer[300];
+    strcpy(buffer, argv[1]);
+    printf("\n\nI'm sorry, my responses are limited. You must ask the right questions.\n\n");
+}
